1f2a10c857647985b1e6883fdd16aef967d5bac2
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-06-20 19:37:26 +0200

    error 500 fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ const fetchFunction = async (url, options = null) => {
     try {
         let res = await fetch (url, options);
         if (!res.ok) {
+            if (res.status === 500) {
+                throw new Error(`HTTP error 500: Internal Server Error`);
+            }
+
             throw new Error(`HTTP error! status: ${res.status}`);
         }
 
@@ -17,6 +21,48 @@ const fetchFunction = async (url, options = null) => {
 const options = {
     // Nothing here
 }
+
+// Calculate CO2 Intensity Data
+
+let calculateInputElm = document.getElementById('calculate-input')
+let calculateInputBtn = document.getElementById('calculate-button')
+let calculateView = document.getElementById('calculate-div')
+
+let calculateCheckElm = document.createElement('p')
+calculateView.appendChild(calculateCheckElm)
+calculateView.setAttribute('id', 'hidden')
+
+calculateInputBtn.addEventListener('click', async (event) => {
+    event.preventDefault()
+
+    let inputtedURL = calculateInputElm.value;
+
+    try {
+    let calculateData = await fetchFunction(`https://api.thegreenwebfoundation.org/api/v3/ip-to-co2intensity/${inputtedURL}`)
+
+    calculateCheckElm.innerHTML = `
+    Carbon Intensity: ${calculateData.carbon_intensity} ${calculateData.carbon_intensity_type}. g/kWh<br>
+    Fossil Fuels: ${calculateData.generation_from_fossil} percent<br>
+    Checked IP/URL: ${calculateData.checked_ip}<br>
+    Country: ${calculateData.country_name} (${calculateData.country_code_iso_2})<br>
+    Year: ${calculateData.year} (Lastest data sample)<br><br>
+    Data is provided by<br> <a href="https://www.thegreenwebfoundation.org/" target="_blank">The Green Web Foundation</a> and <a href="https://ember-climate.org/data/data-explorer/" target="_blank">Ember</a>
+    `
+    }
+     catch (error) {
+        if (error.message.includes('500')) {
+            calculateCheckElm.innerHTML = `An error occurred:<br> ${error.message}`;
+        } else {
+            calculateCheckElm.innerHTML = `An unexpected error occured:<br> ${error.message}`
+        }
+    }
+
+    calculateView.appendChild(calculateCheckElm)
+    calculateView.setAttribute('id', 'calculate-div')
+})
+
+// --------------------------------------------------
+
 
 // Check whether Green host or not
 let greenInputElm = document.getElementById('greencheck-input')
@@ -45,9 +91,6 @@ greenInputBtn.addEventListener('click', async (event) => {
     greenCheckView.setAttribute('id', 'greencheck-div')
 })
 
-
-
-
 // Green
 // ${greenCheckData.green} - Green: True
 // ${greenCheckData.url} - Provided URL
@@ -59,37 +102,3 @@ greenInputBtn.addEventListener('click', async (event) => {
 // ${greenCheckData.green} - Green: False
 // ${greenCheckData.url} - Provided URL
 // ${greenCheckData.data} - Data: False
-
-
-
-// --------------------------------------------------
-
-// Calculate CO2 Intensity Data
-
-let calculateInputElm = document.getElementById('calculate-input')
-let calculateInputBtn = document.getElementById('calculate-button')
-let calculateView = document.getElementById('calculate-div')
-
-let calculateCheckElm = document.createElement('p')
-calculateView.appendChild(calculateCheckElm)
-calculateView.setAttribute('id', 'hidden')
-
-calculateInputBtn.addEventListener('click', async (event) => {
-    event.preventDefault()
-
-    let inputtedURL = calculateInputElm.value;
-
-    let calculateData = await fetchFunction(`https://api.thegreenwebfoundation.org/api/v3/ip-to-co2intensity/${inputtedURL}`)
-
-    calculateCheckElm.innerHTML = `
-    Carbon Intensity: ${calculateData.carbon_intensity} ${calculateData.carbon_intensity_type}. grams per kilowatt-hour (g/kWh)<br>
-    Generation From Fossil: ${calculateData.generation_from_fossil} percent generated from fossil fuels<br>
-    Checked IP/URL: ${calculateData.checked_ip}<br>
-    Country: ${calculateData.country_name} (${calculateData.country_code_iso_2})<br>
-    Year: ${calculateData.year} (Lastest data sample)<br><br>
-    Data is provided by<br> <a href="https://www.thegreenwebfoundation.org/" target="_blank">The Green Web Foundation</a> and <a href="https://ember-climate.org/data/data-explorer/" target="_blank">Ember</a>
-    `
-
-    calculateView.appendChild(calculateCheckElm)
-    calculateView.setAttribute('id', 'calculate-div')
-})
