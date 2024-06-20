ea9ec1b14c4b4285b7fd9c3a1611693573c3c90e
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-06-20 22:19:49 +0200

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Async Fetch Function - Try fetch, if Response OK, get Data. Catch error, show error in console
 const fetchFunction = async (url, options = null) => {
     try {
         let res = await fetch (url, options);
@@ -22,7 +23,7 @@ const options = {
     // Nothing here
 }
 
-// Calculate CO2 Intensity Data
+// -------------------- Calculate CO2 Intensity Data --------------------
 
 let calculateInputElm = document.getElementById('calculate-input')
 let calculateInputBtn = document.getElementById('calculate-button')
@@ -32,14 +33,17 @@ let calculateCheckElm = document.createElement('p')
 calculateView.appendChild(calculateCheckElm)
 calculateView.setAttribute('id', 'hidden')
 
+// Calculate button event listener - Takes value from input field and uses it in fetch ${inputtedURL}
+// Builds view, otherwise shows an error
 calculateInputBtn.addEventListener('click', async (event) => {
-    event.preventDefault()
+    event.preventDefault() // Prevents default action of form/button
 
     let inputtedURL = calculateInputElm.value;
 
     try {
     let calculateData = await fetchFunction(`https://api.thegreenwebfoundation.org/api/v3/ip-to-co2intensity/${inputtedURL}`)
 
+    // InnerHTML for #calculate-div
     calculateCheckElm.innerHTML = `
     Carbon Intensity: ${calculateData.carbon_intensity} ${calculateData.carbon_intensity_type}. g/kWh<br>
     Fossil Fuels: ${calculateData.generation_from_fossil} percent<br>
@@ -47,7 +51,7 @@ calculateInputBtn.addEventListener('click', async (event) => {
     Country: ${calculateData.country_name} (${calculateData.country_code_iso_2})<br>
     Year: ${calculateData.year} (Lastest data sample)<br><br>
     Data is provided by<br> <a href="https://www.thegreenwebfoundation.org/" target="_blank">The Green Web Foundation</a> and <a href="https://ember-climate.org/data/data-explorer/" target="_blank">Ember</a>
-    `
+    ` // End of InnerHTML
     }
      catch (error) {
         if (error.message.includes('500')) {
@@ -61,8 +65,8 @@ calculateInputBtn.addEventListener('click', async (event) => {
     calculateView.setAttribute('id', 'calculate-div')
 })
 
-// --------------------------------------------------
 
+// -------------------- Green checker code --------------------
 
 // Check whether Green host or not
 let greenInputElm = document.getElementById('greencheck-input')
@@ -73,22 +77,58 @@ let greenCheckElm = document.createElement('p')
 greenCheckView.appendChild(greenCheckElm)
 greenCheckView.setAttribute('id', 'hidden')
 
+// Green check button event listener - Takes value from input field and uses it in fetch ${inputtedURL}
+// Builds view, otherwise shows an error
 greenInputBtn.addEventListener('click', async (event) => {
-    event.preventDefault()
+    event.preventDefault() // Prevents default action of form/button
+
     let inputtedURL = greenInputElm.value;
 
-    let greenCheckData = await fetchFunction(`https://admin.thegreenwebfoundation.org/api/v3/greencheck/${inputtedURL}`)
+    greenCheckView.innerHTML = ''; // Clears view before build
+    greenCheckView.appendChild(greenCheckElm);
 
-    // console.log(greenCheckData);
+    try {
+        let greenCheckData = await fetchFunction(`https://admin.thegreenwebfoundation.org/api/v3/greencheck/${inputtedURL}`)
 
-    if (greenCheckData.green) {
-        greenCheckElm.innerHTML = `Would you look at that, the provided URL (${greenCheckData.url}) is hosted Green.<br><img src="https://www.svgrepo.com/show/356736/checkmark.svg">`;
-    } else {
-        greenCheckElm.innerHTML = `We can't confirm that the provided URL (${greenCheckData.url}) is hosted Green.<br><img src="https://www.svgrepo.com/show/511674/close-1511.svg">`;
+        // console.log(greenCheckData);
+
+        // if green = true, build view
+        if (greenCheckData.green) {
+            // InnerHTML for #greencheck-div
+            greenCheckElm.innerHTML = `
+            Would you look at that, the provided URL (${greenCheckData.url}) is hosted Green.<br>
+            <img src="https://www.svgrepo.com/show/356736/checkmark.svg"><br><br>
+            The site is hosted by:<br> <a href="${greenCheckData.hosted_by_website}" target="_blank">${greenCheckData.hosted_by}</a><br><br>See supporting documents below:
+            ` // End of InnerHTML
+
+            // if green = true, data = true - Therefore documents are available
+            // forEach loop for document build for docElm
+            if (Array.isArray(greenCheckData.supporting_documents)) {
+                greenCheckData.supporting_documents.forEach(supporting_documents => {
+                    let docElm = document.createElement('div')
+                    // docElm innerHTML
+                    docElm.innerHTML = `
+                    <a href="${supporting_documents.link}" target="_blank">${supporting_documents.title}</a>
+                    ` // End of docElm innerHTML
+
+                    greenCheckView.appendChild(docElm);
+                })
+            }
+            // if green = false, build view
+        } else {
+            // false view innerHTML
+            greenCheckElm.innerHTML = `
+            We can't confirm that the provided URL (${greenCheckData.url}) is hosted Green.<br><img src="https://www.svgrepo.com/show/511674/close-1511.svg">
+            ` // End of false view innerHTML
+        }
+
+        // greenCheckView.appendChild(greenCheckElm)
+        greenCheckView.setAttribute('id', 'greencheck-div')
+        
+    } catch (error) {
+        greenCheckElm.innerHTML = `An error occured: ${error.message}`
+        greenCheckElm.setAttribute('id', 'greencheck-div')
     }
-
-    greenCheckView.appendChild(greenCheckElm)
-    greenCheckView.setAttribute('id', 'greencheck-div')
 })
 
 // Green
